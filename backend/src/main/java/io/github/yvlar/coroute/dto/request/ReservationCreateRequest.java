be833e49a7fc657353a23ba56eb225b939e031e4
Yves Lariviere <118058767+yvlar@users.noreply.github.com>
2026-07-22 16:56:37 -0400

    Limiter le nombre de places réservables

--- backend/src/main/java/io/github/yvlar/coroute/dto/request/ReservationCreateRequest.java
+++ backend/src/main/java/io/github/yvlar/coroute/dto/request/ReservationCreateRequest.java
@@ -1,5 +1,6 @@
 package io.github.yvlar.coroute.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-public record ReservationCreateRequest(@Min(1) int nombrePlaces) {}
+public record ReservationCreateRequest(@Min(1) @Max(8) int nombrePlaces) {}
